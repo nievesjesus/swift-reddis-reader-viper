3a3a1231f4b3c5e5dc9faef39697194a60e936b2
JesusN <yo@nievesjesus.com>
2021-05-24 10:24:56 -0300

    project setup

---
+++ Viper-Reddis-Reader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Viper-Reddis-Reader
+//
+//  Created by Jesus Nieves on 24/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Viper-Reddis-ReaderTests/Viper_Reddis_ReaderTests.swift
@@ -0,0 +1,33 @@
+//
+//  Viper_Reddis_ReaderTests.swift
+//  Viper-Reddis-ReaderTests
+//
+//  Created by Jesus Nieves on 24/05/2021.
+//
+
+import XCTest
+@testable import Viper_Reddis_Reader
+
+class Viper_Reddis_ReaderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
